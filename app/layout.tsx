33e756d548c7fdb64342748e2ebfd6e479f45f42
Jiwung <jiwung1994@gmail.com>
2025-05-07 15:56:50 +0900

    구글 애드센스 meta 태그 추가: google-adsense-account

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-2932774121154983" />
+      </head>
       <body>{children}</body>
     </html>
   );
